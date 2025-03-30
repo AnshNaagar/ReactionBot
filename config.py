b0b02aec1779f8cef0b07c501ab0748512ea83b3
AnshNaagar <158273761+AnshNaagar@users.noreply.github.com>
2025-03-30 14:35:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+BOT_TOKEN = getenv("BOT_TOKEN", "7978909661:AAG0PFsC0EaeY1ZRqIZCO1S-mR91NEWi2DM")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Ansh1:TGwow@ansh1.35aemhh.mongodb.net/?retryWrites=true&w=majority&appName=Ansh1")
+OWNER_ID = int(getenv("OWNER_ID", "7508448025"))
+SUPPORT_GRP = "Supreme_XD"
+UPDATE_CHNL = "Supreme_XD"
+OWNER_USERNAME = "Supreme_XD"
     
